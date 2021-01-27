833a9452a9d64db846373007b9dd9f3b8d5c347d
Kuaierr <1063783237@qq.com>
2021-01-27 15:46:39 +0800

    camera

---
+++ Assets/Scripts/Camera/CameraDragManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDragManager : MonoBehaviour
+{
+    public float sensitivityAmt = 1f;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+
+            Vector3 p0 = transform.position;
+            Vector3 p01 = p0 - transform.right * Input.GetAxisRaw("Mouse X") * sensitivityAmt * Time.timeScale;
+            Vector3 p03 = p01 - transform.up * Input.GetAxisRaw("Mouse Y") * sensitivityAmt * Time.timeScale;
+            transform.position = p03;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject subCamera;
+    private GameObject v1;
+    private GameObject v2;
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        subCamera = GameObject.Find("Sub Camera");
+        v1 = GameObject.Find("S vcam1");
+        v2 = GameObject.Find("L vcaml");
+        subCamera.SetActive(false);
+        v1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            mainCamera.SetActive(false);
+            subCamera.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mainCamera.SetActive(true);
+            subCamera.SetActive(false);
+        }
+
+        if (mainCamera)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                v1.SetActive(true);
+                //v2.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                v1.SetActive(false);
+                //v2.SetActive(true);
+            }
+        }
+
+    }
+}
